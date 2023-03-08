2c51cee998e714eab8bdf4a7241dc9623f5df776
Jack Elliott <jackthename@gmail.com>
2023-03-07 23:37:09 -0800

    updated to use model gpt-3.5-turbo-0301

--- index.js
+++ index.js
@@ -3,33 +3,33 @@ require("dotenv").config();
 
 // Require the necessary discord.js classes
 const {
-    Client,
-    Collection,
-    GatewayIntentBits,
-    REST,
-    Partials,
+  Client,
+  Collection,
+  GatewayIntentBits,
+  REST,
+  Partials,
 } = require("discord.js");
 
 // Create a new client instance
 const client = new Client({
-    intents: [
-        GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.MessageContent
-    ],
-    partials: [
-        Partials.User, // We want to receive uncached users!
-        Partials.Channel,
-        Partials.Message,
-    ],
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+  partials: [
+    Partials.User, // We want to receive uncached users!
+    Partials.Channel,
+    Partials.Message,
+  ],
 });
-const rest = new REST({version: "10"}).setToken(process.env.DISCORD_TOKEN);
+const rest = new REST({ version: "10" }).setToken(process.env.DISCORD_TOKEN);
 
 // Prepare the OpenAI API connection
-const {Configuration, OpenAIApi} = require("openai");
+const { Configuration, OpenAIApi } = require("openai");
 const configuration = new Configuration({
-    apiKey: process.env.OPENAI_KEY,
-    organization: process.env.OPENAI_ORG,
+  apiKey: process.env.OPENAI_KEY,
+  organization: process.env.OPENAI_ORG,
 });
 const openai = new OpenAIApi(configuration);
 
@@ -41,25 +41,29 @@ client.tasks = [];
 // Log in to Discord with your client's token
 client.login(process.env.DISCORD_TOKEN);
 
-client.on('messageCreate', async function (message) {
-    try {
-        if (message.author.bot) return;
-        const gptResponse = await openai.createCompletion({
-            model: "text-davinci-003",
-            prompt: `ChatGPT is a friendly and informative chatbot.\n\ChatGPT: Hello, how are you?\n\${message.author.username}: ${message.content}\n\ChatGPT:`,
-            temperature: 0.9,
-            max_tokens: 100,
-            stop: ['ChatGPT:']
-        })
+client.on("messageCreate", async function (message) {
+  try {
+    if (message.author.bot) return;
+    const gptResponse = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo-0301",
+      messages: [
+        {
+          role: "system",
+          content: `You are a friendly and informative chatbot assistant.\n\ChatGPT: Hello, how are you?\n\${message.author.username}: ${message.content}\n\ChatGPT:`,
+        },
+        { role: "user", content: `${message.content}` },
+      ],
+      temperature: 0.9,
+      max_tokens: 100,
+    });
 
-        message.reply(`${gptResponse.data.choices[0].text}`);
-
-    } catch (error) {
-        if (error.response) {
-            console.log(error.response.status);
-            console.log(error.response.data);
-        } else {
-            console.log(error.message);
-        }
+    message.reply(`${gptResponse.data.choices[0].message.content}`);
+  } catch (error) {
+    if (error.response) {
+      console.log(error.response.status);
+      console.log(error.response.data);
+    } else {
+      console.log(error.message);
     }
+  }
 });
